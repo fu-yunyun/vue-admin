f073aeab12a1b5b3ae17ef6b83ed9ab824d0675b
Your Name <you@example.com>
2021-04-15 19:08:24 +0800

    s

---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="login">
+    This is a login page <br />
+
+    <!-- <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  // vh:基于可视区的百分比
+  height: 100vh;
+  background-color: #3445af;
+}
+</style>
